052af7db28d83efeae293c307dfd7560f886e6e5
Nicolas Miguel Uczak <nicolasmiuczak@gmail.com>
2025-05-05 20:25:44 -0300

    Mais aula 02

--- JAVASCRIPT/AULA 02/script.js
+++ JAVASCRIPT/AULA 02/script.js
@@ -3,6 +3,10 @@ const mensagem = document.querySelector(".msg");
 const paragrafo = document.querySelectorAll("p");
 const botao = document.getElementById("botao");
 
+const inputNome = document.getElementById("nome");
+const enviar = document.getElementById("enviar");
+const saudacao = document.getElementById("saudacao");
+
 botao.addEventListener("click", function(){
     titulo.innerText = "Você clicou no botão!";
     document.body.style.backgroundColor = "#ffa500";
@@ -10,5 +14,31 @@ botao.addEventListener("click", function(){
     paragrafo.forEach(function(p){
         p.innerText += "✅"; 
     })
+})
+
+
+function exibirSaudacao() {
+    const nome = inputNome.value;
+    saudacao.innerText = `Olá, ${nome}! Seja bem-vindo(a) ao nosso site!`;
+}
+
+enviar.addEventListener("click", exibirSaudacao);
+
+enviar.addEventListener("mouseover", function(){
+    enviar.style.backgroundColor = "#4CAF50"; // Verde
+    enviar.style.color = "white";
+})
+
+inputNome.addEventListener("input", function(){
+//    alert("Texto sendo digitado");
+})
+
+inputNome.addEventListener("keydown", function(event){
+    alert(`A tecla foi pressionada ${event.key}`);
+})
+
+enviar.addEventListener("mouseout", function(){
+    enviar.style.backgroundColor = "#f0f0f0";
+    enviar.style.color = "black";
 
 })
